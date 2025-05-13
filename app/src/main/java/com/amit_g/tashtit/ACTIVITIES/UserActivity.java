b2c0e30e38d18fa1755ec5c42e9559c4ac120da1
user <amitgilat2007@gmail.com>
2025-05-13 23:51:44 +0300

    vcs

--- app/src/main/java/com/amit_g/tashtit/ACTIVITIES/UserActivity.java
+++ app/src/main/java/com/amit_g/tashtit/ACTIVITIES/UserActivity.java
@@ -1,10 +1,14 @@
 package com.amit_g.tashtit.ACTIVITIES;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.lifecycle.ViewModelProvider;
@@ -13,7 +17,6 @@ import com.amit_g.helper.inputValidators.EmailRule;
 import com.amit_g.helper.inputValidators.EntryValidation;
 import com.amit_g.helper.inputValidators.NameRule;
 import com.amit_g.helper.inputValidators.PasswordRule;
-import com.amit_g.helper.inputValidators.Rule;
 import com.amit_g.helper.inputValidators.RuleOperation;
 import com.amit_g.helper.inputValidators.TextRule;
 import com.amit_g.helper.inputValidators.Validator;
@@ -24,11 +27,10 @@ import com.amit_g.viewmodel.UsersViewModel;
 
 public class UserActivity extends BaseActivity implements EntryValidation {
 
-    private EditText etUserName;
-    private EditText etPassword;
-    private EditText etEmail;
+    private EditText etUserName, etPassword, etEmail;
     private Button btnSignUp;
     private CheckBox cbNotification;
+    private TextView tvLogin;
     private User oldUser;
     private UsersViewModel viewModel;
 
@@ -39,9 +41,9 @@ public class UserActivity extends BaseActivity implements EntryValidation {
         setContentView(R.layout.activity_user2);
 
         initializeViews();
-        setListeners();
         setViewModel();
-        setValidation();  // Ensure validation rules are set here
+        setValidation();
+        setListeners();
     }
 
     @Override
@@ -51,29 +53,35 @@ public class UserActivity extends BaseActivity implements EntryValidation {
         etPassword = findViewById(R.id.etPassword);
         cbNotification = findViewById(R.id.cbNotification);
         btnSignUp = findViewById(R.id.btnSignUp);
-
-        setListeners();
+        tvLogin = findViewById(R.id.tvLogin);
     }
 
     @Override
     protected void setListeners() {
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (validate()) {
-                    User user = new User();
-                    user.setUserName(etUserName.getText().toString());
-                    user.setEmail(etEmail.getText().toString());
-                    user.setPassword(etPassword.getText().toString());
-                    user.setNotifications(cbNotification.isChecked());
+        btnSignUp.setOnClickListener(v -> {
+            if (validate()) {
+                User user = new User();
+                user.setUserName(etUserName.getText().toString());
+                user.setEmail(etEmail.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+                user.setNotifications(cbNotification.isChecked());
 
-                    if (oldUser != null) {
-                        user.setIdFs(oldUser.getIdFs());
-                    }
-                    viewModel.save(user);
+                if (oldUser != null) {
+                    user.setIdFs(oldUser.getIdFs());
                 }
+
+                String username = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                viewModel.save(user);
+                User loggedInUser = user;
+                saveUserToPreferences(loggedInUser);
+                navigateToActivity(HomeActivity.class);
+
             }
         });
+
+        tvLogin.setOnClickListener(v -> navigateToActivity(LoginActivity.class));
     }
 
     @Override
@@ -81,30 +89,30 @@ public class UserActivity extends BaseActivity implements EntryValidation {
         viewModel = new ViewModelProvider(this).get(UsersViewModel.class);
     }
 
-//    @Override
-//    public void setValidation() {
-//        Validator.add(new Rule(etUserName, RuleOperation.REQUIRED, "Username is required"));
-//        Validator.add(new Rule(etUserName, RuleOperation.NAME, "Username is not valid"));
-//        Validator.add(new Rule(etEmail, RuleOperation.REQUIRED, "Email is required"));
-//        Validator.add(new Rule(etEmail, RuleOperation.TEXT, "Email is not valid"));
-//        Validator.add(new Rule(etPassword, RuleOperation.REQUIRED, "Password is required"));
-//        Validator.add(new Rule(etPassword, RuleOperation.PASSWORD, "Password is not valid"));
-//
-//    }
-@Override
-public void setValidation() {
-    // Assuming TextRule and NameRule are subclasses of Rule
-    Validator.add(new TextRule(etUserName, RuleOperation.REQUIRED, "Username is required"));
-    Validator.add(new NameRule(etUserName, RuleOperation.NAME, "Username is not valid"));
-    Validator.add(new EmailRule(etEmail, RuleOperation.REQUIRED, "Email is required"));
-    Validator.add(new EmailRule(etEmail, RuleOperation.TEXT, "Email is not valid"));
-    Validator.add(new PasswordRule(etPassword, RuleOperation.REQUIRED, "Password is required"));
-    //Validator.add(new PasswordRule(etPassword, RuleOperation.PASSWORD, "Password is not valid"));
-}
+    private void saveUserToPreferences(User user) {
+        SharedPreferences sharedPref = getApplicationContext()
+                .getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("userIdFs", user.getIdFs());
+        editor.putString("username", user.getUserName());
+        editor.putString("email", user.getEmail());
+        // Add more fields if necessary
+        editor.apply();
+    }
 
+    @Override
+    public void setValidation() {
+        Validator.add(new TextRule(etUserName, RuleOperation.REQUIRED, "Username is required"));
+        Validator.add(new NameRule(etUserName, RuleOperation.NAME, "Username is not valid"));
+        Validator.add(new EmailRule(etEmail, RuleOperation.REQUIRED, "Email is required"));
+        Validator.add(new EmailRule(etEmail, RuleOperation.TEXT, "Email is not valid"));
+        Validator.add(new PasswordRule(etPassword, RuleOperation.REQUIRED, "Password is required"));
+        // Uncomment below if you have custom PASSWORD rule implemented
+        // Validator.add(new PasswordRule(etPassword, RuleOperation.PASSWORD, "Password is not valid"));
+    }
 
     @Override
     public boolean validate() {
-        return Validator.validate();  // Check all rules and return the result
+        return Validator.validate();
     }
 }
